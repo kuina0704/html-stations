eb19b6fe82ab57fc517aa5c376d2c124ec8d18f8
kuina0704 <route2601z@yahoo.co.jp>
2024-04-10 14:22:47 +0900

    commmit

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,19 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+async function test(name) {
+  return await new Promise ((resolve) => {
+    setTimeout(() => {
+      const buildFullName = name.map((data) => {
+        data.full_name = `${data.family_name} ${data.first_name}`;
+        return data;
+      });
+      resolve(buildFullName);
+    }, 3000);
+  });
 }
 
